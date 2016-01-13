5f7a5edfff482c40180b0aac542df7933b76ae07
Mark St.Godard <markstgodard@gmail.com>
2016-01-13 15:31:17 -0600

    Validate Host header characters

--- proxy/request_handler.go
+++ proxy/request_handler.go
@@ -326,23 +326,3 @@ func forwardIO(a, b net.Conn) {
 
 	<-done
 }
-
-func onlyAllowedHostCharacters(host string) bool {
-	for i := 0; i < len(host); i++ {
-		c := host[i]
-		if !isHostCharacterAllowed(c) {
-			return false
-		}
-	}
-	return true
-}
-
-func isHostCharacterAllowed(c byte) bool {
-	/* To check valid host characters, refer to:
-	   - Section 3.1 of RFC 1738
-	   - Section 3.5 of RFC 1034
-	   - Section 2.1 of RFC 1123 */
-	return ' ' == c || '-' == c || '.' == c ||
-		('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') ||
-		('0' <= c && c <= '9')
-}
